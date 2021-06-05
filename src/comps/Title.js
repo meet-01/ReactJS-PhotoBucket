a722c4790a99e2f3370b98efd14d0563a41949bf
Meet Master <meetmaster16@gmail.com>
2021-06-05 23:51:13 +0530

    error change

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -7,7 +7,8 @@ const Title = () => {
       <h2> Pictures</h2>
       <p>
         You make memories, we save it for you to relive again and again! Photo
-        Bucket is the place to store all your memories. Click, share, relive!
+        Bucket is the place to store all your memories. just click, share,
+        relive!
       </p>
     </div>
   );
